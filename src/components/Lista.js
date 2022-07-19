abbb5c8ffde8c14f3f2ed804c45ab4a707dc91ad
Enrique Palacios <enriquepalaciosc@gmail.com>
2022-07-19 14:39:26 -0400

    Añadidas rutas y elementos base vacíos

---
+++ src/components/Lista.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+
+const Lista = () => {
+
+    return (
+        <div className="container">
+            TEST
+        </div>
+    )
+}
+
+export default Lista
